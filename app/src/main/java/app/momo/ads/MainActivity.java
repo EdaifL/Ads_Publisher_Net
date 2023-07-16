2337d4618973d0f869915171619cd912eedbd3b2
LAP <edaiflahoucine7@gmail.com>
2023-07-16 12:15:19 +0100

    init

--- app/src/main/java/app/momo/ads/MainActivity.java
+++ app/src/main/java/app/momo/ads/MainActivity.java
@@ -3,6 +3,10 @@ package app.momo.ads;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import app.momo.multi_publisher_net.Ads.Ads;
+import app.momo.multi_publisher_net.Tools.CpaAsNative;
 
 public class MainActivity extends AppCompatActivity {
 
